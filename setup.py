5cf410f48e990e15b04262d1849b5da882ce5940
amir <amirhosseinsedaghati42@gmail.com>
2023-08-11 16:34:46 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    f.close()
+    
+setup(
+    name='pyimkernel',
+    version='0.3.0',
+    packages=find_packages(exclude=['tests*', 'examples*']),
+    license='MIT',
+    description='Apply image kernel(s) on a image',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['numpy', 'matplotlib'],
+    url='https://github.com/amirho3einsedaghati/pyimkernel',
+    author='Amir Hosein Sedaghati',
+    author_email='amirhosseinsedaghati42@gmail.com'
+)
