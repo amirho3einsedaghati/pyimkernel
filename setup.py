5efecf52392f70a8d4de65accbf31a7dae0986fe
amir <amirhosseinsedaghati42@gmail.com>
2023-08-12 02:19:07 -0400

    add some metadata

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ with open("README.md", "r", encoding="utf-8") as f:
     
 setup(
     name='pyimkernel',
-    version='0.4.0',
+    version='0.5.0',
     packages=find_packages(exclude=['tests*', 'examples*']),
     license='MIT',
-    description='Apply image kernel(s) on a image',
+    description='Applying some image kernel(s) on an image',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=['numpy', 'matplotlib'],
+    install_requires=['numpy', 'matplotlib', 'cv2'],
     url='https://github.com/amirho3einsedaghati/pyimkernel',
     author='Amir Hosein Sedaghati',
-    author_email='amirhosseinsedaghati42@gmail.com'
+    author_email='amirhosseinsedaghati42@gmail.com',
+    author_contact='https://linktr.ee/amirhoseinsedaghati',
+    classifiers=['Programming Language :: Python :: 3'],
+    keywords=['image kernels/filters', 'image preprocessing', 'Applying image kernels/filters on a grayscale or color scale image']
 )
