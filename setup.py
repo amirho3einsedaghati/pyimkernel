8788c88b21611e3f263f53cb42761499916cec85
amir <amirhosseinsedaghati42@gmail.com>
2023-09-15 02:50:16 -0400

    change version into the 0.8.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='pyimkernel',
-    version='0.8.2',
+    version='0.8.3',
     packages=find_packages(exclude=['tests*', 'examples*']),
     license='MIT',
     description='Applying some image kernel(s) on a grayscale or RGB color-scale image',
