df9359d959410398008f0812817d78264998e6c0
amir <amirhosseinsedaghati42@gmail.com>
2023-12-15 17:43:37 -0500

    Update __init__.py

--- pyimkernel/__init__.py
+++ pyimkernel/__init__.py
@@ -1,1 +1,5 @@
-from .main import ApplyKernels
+from .main import ApplyKernels, ManipulatePixels
+from pyimkernel.builtin_kernels import kernels
+
+import pkg_resources
+__version__ = pkg_resources.get_distribution('pyimkernel').version
